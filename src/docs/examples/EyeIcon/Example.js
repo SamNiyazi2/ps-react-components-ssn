c540f3c5922edd28311a7649f8952ebc572285bd
Sam Niyazi <sam2003@niyazi.com>
2020-06-28 23:53:16 -0500

    Completed M06

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,11 @@
+
+// 06/28/2020 11:47 pm - SSN - [20200628-2339] - [002] - M06 - Demo Icon atom
+
+import React from 'react';
+import EyeIcon from 'ps-react2/EyeIcon';
+
+
+export default function EyeIconExample() { 
+    return <EyeIcon/>
+}
+ 
